e664c6954e02236386d3226fce89111df3855e3d
Marie-Elise <membrq@gmail.com>
2017-01-30 19:52:29 -0600

    Checkpoint 6 completed. Added Fixtures service.

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,9 @@
 (function() {
-     function AlbumCtrl() {
-		this.albumData = (angular.copy(albumPicasso));
+     function AlbumCtrl(Fixtures) {
+		this.albumData = Fixtures.getAlbum();
      }
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', ['albumPicasso', AlbumCtrl]);
+         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
  })();
