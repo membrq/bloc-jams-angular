038a5c221a93b23c77a3aed634ab699de076bdb9
Marie-Elise <membrq@gmail.com>
2018-01-21 22:45:50 -0600

    Updated README.md with project description

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,28 +5,27 @@
              enabled: true,
              requireBase: false
          });
-		 
+
 		 $stateProvider
          .state('landing', {
              url: '/',
 			 controller: 'LandingCtrl as landing',
              templateUrl: '/templates/landing.html'
          })
-		  
+
          .state('album', {
              url: '/album',
 			 controller: 'AlbumCtrl as album',
              templateUrl: '/templates/album.html'
          })
-		  
+
 		 .state('collection', {
              url: '/collection',
 			 controller: 'CollectionCtrl as collection',
              templateUrl: '/templates/collection.html'
          });
      }
- 
- angular.module('blocJams', ['ui.router']);
+
      angular
          .module('blocJams', ['ui.router'])
          .config(config);
